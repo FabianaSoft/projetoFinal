72d3710982471c44ee8058d7189c5f26bdea7db0
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-08-27 21:55:25 -0300

    adiciona o prime react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-
+import { Button } from 'primereact/button'
 import './App.css'
 
 function App() {
 
+  <h1 className='lg:bg-[#0096D6] md:bg-orange-400 sm:bg-green-400 bg-slate-400 px-4 py-8 border-4 border-solid rounded border-black text-white'>Projeto Final 2024</h1>
+
   return (
     <>
-      <h1 className='bg-orange-400'>Projeto Final 2</h1>
+      <h1 className='bg-orange-400 p-4 text-white border-4 border-solid rounded  '>Projeto Final 2</h1>
+      <Button 
+      label="Submit"
+       icon="pi pi-heart-fill"
+       severity='danger'
+       />
+    
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { PrimeReactProvider } from 'primereact/api'
+import 'primeicons/primeicons.css'
+
+
 import App from './App.tsx'
 
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>,
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
