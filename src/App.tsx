ec61b9c6e987c8e02e899a358258aa1547d32c31
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-08-29 15:56:22 -0300

    importar rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { Button } from 'primereact/button'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-
-  <h1 className='lg:bg-[#0096D6] md:bg-orange-400 sm:bg-green-400 bg-slate-400 px-4 py-8 border-4 border-solid rounded border-black text-white'>Projeto Final 2024</h1>
-
   return (
-    <>
-      <h1 className='bg-orange-400 p-4 text-white border-4 border-solid rounded  '>Projeto Final 2</h1>
-      <Button 
-      label="Submit"
-       icon="pi pi-heart-fill"
-       severity='danger'
-       />
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/departamentos' element={<p>listagem</p>} />
+        <Route path='/departamentos/new' element={<p>new</p>} />
+        <Route path='/departamentos/edit/:id' element={<p>id</p>} />
+        <Route path='*' element={<p>notFound</p>} />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import { PrimeReactProvider } from 'primereact/api'
 import 'primeicons/primeicons.css'
-
-
 import App from './App.tsx'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
