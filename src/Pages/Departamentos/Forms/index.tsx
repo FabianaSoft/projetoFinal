17d679cce67f1d94764ea513dfd8d7737fbca0d1
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-09-16 15:57:51 -0300

    inserir botoes

--- src/Pages/Departamentos/Forms/index.tsx
+++ src/Pages/Departamentos/Forms/index.tsx
@@ -7,28 +7,40 @@ import { insereDepartamento } from "../../../Services/Departamentos/insereDepart
 function Forms() {
     const [nome, setNome] = useState("")
     const [sigla, setSigla] = useState("")
+    const [nomeError, setNomeError] = useState(false)
+    const [siglaError, setSiglaError] = useState(false)
 
-    async function inserir(){
-        const departamento = {sigla, nome}
+    async function inserir() {
+        if (nome === "" || sigla === "") {
+            setNomeError(nome === "")
+            setSiglaError(sigla === "")
+        }
+        const departamento = { sigla, nome }
         const result = await insereDepartamento(departamento)
         console.log(result)
+        setNomeError(false)
+        setSiglaError(false)
 
-    } 
+    }
     return (
         <div className="col-span-12">
             <div className="flex w-full gap-2">
                 <div className="w-2/3">
                     <InputText placeholder="Nome" className="w-full" onChange={e => setNome(e.target.value)} />
+                    {nomeError && <small className="text-red-600">
+                        Nome inválido
+                    </small>}
                 </div>
                 <div className="w-1/3">
                     <InputText placeholder="Sigla" className="w-full" onChange={e => setSigla(e.target.value)} />
+                    {siglaError && <small className="text-red-600">
+                        Sigla inválida
+                    </small>}
                 </div>
 
             </div>
             <Button label="Salvar" icon="pi pi-save" className="mt-2" onClick={inserir} />
-            <small className="text-red-600">
-                Nome inválido
-            </small>
+
         </div>
     )
 }
--- src/Pages/Departamentos/Listagem/index.tsx
+++ src/Pages/Departamentos/Listagem/index.tsx
@@ -1,49 +1,52 @@
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 import { Button } from 'primereact/button'
+import { useNavigate } from 'react-router-dom';
 
 const Departamentos = () => {
-    
+    const navigate = useNavigate()
 
-  const departamentos = [
 
-    {
-        
-      id_departamento: 1,
-      nome: 'Recursos Humanos',
-      sigla: 'RH'
-    },
-    {
-      id_departamento: 2,
-      nome: 'Tecnologia da Informação',
-      sigla: 'TI'
-    },
-  ]
-  const bodyAcao = () => {
+    const departamentos = [
+
+        {
+
+            id_departamento: 1,
+            nome: 'Recursos Humanos',
+            sigla: 'RH'
+        },
+        {
+            id_departamento: 2,
+            nome: 'Tecnologia da Informação',
+            sigla: 'TI'
+        },
+    ]
+    const bodyAcao = () => {
+        return (
+            <>
+                <Button icon="pi pi-pencil" rounded severity='warning' className='mr-2' />
+                <Button icon="pi pi-trash" rounded severity='danger' />
+            </>
+        )
+    }
+
     return (
         <>
-        <Button icon="pi pi-pencil" rounded severity='warning' className='mr-2' /> 
-        <Button icon="pi pi-trash" rounded severity='danger'/>
-      </>
+            <div className="col-span-12 flex justify-between items-center">
+                <h1>Listagem de Departamentos</h1>
+                <Button label="novo" icon="pi pi-plus" rounded onClick={()=>navigate("/departamentos/new")} />
+            </div>
+
+            <div className="col-span-12">
+                <DataTable value={departamentos}>
+                    <Column field="sigla" header="Sigla" />
+                    <Column field="nome" header="Nome" />
+                    <Column header="Ação" body={bodyAcao} />
+
+                </DataTable>
+            </div>
+        </>
     )
-  }
-
-  return (
-    <>
-      <div className="col-span-12">
-        <h1>Listagem de Departamentos</h1>
-      </div>
-
-      <div className="col-span-12">
-        <DataTable value={departamentos}>
-        <Column field="sigla" header="Sigla"/>
-        <Column field="nome" header="Nome"/>
-        <Column header="Ação" body={bodyAcao} />
-          
-        </DataTable>
-      </div>
-    </>
-  )
 };
 
 export default Departamentos;
