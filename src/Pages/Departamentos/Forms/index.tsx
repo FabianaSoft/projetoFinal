a3dcb9e07c706c496797e4137c267a0219f145b0
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-09-18 15:30:51 -0300

    inserir botao voltar

--- src/Pages/Departamentos/Forms/index.tsx
+++ src/Pages/Departamentos/Forms/index.tsx
@@ -2,14 +2,17 @@ import { InputText } from "primereact/inputtext"
 import { Button } from "primereact/button"
 import { useState } from "react"
 import { insereDepartamento } from "../../../Services/Departamentos/insereDepartamento"
+import {useNavigate} from "react-router-dom"
 
 
 function Forms() {
+    const navigate = useNavigate()
     const [nome, setNome] = useState("")
     const [sigla, setSigla] = useState("")
     const [nomeError, setNomeError] = useState(false)
     const [siglaError, setSiglaError] = useState(false)
 
+    
     async function inserir() {
         if (nome === "" || sigla === "") {
             setNomeError(nome === "")
@@ -23,25 +26,31 @@ function Forms() {
 
     }
     return (
-        <div className="col-span-12">
-            <div className="flex w-full gap-2">
-                <div className="w-2/3">
-                    <InputText placeholder="Nome" className="w-full" onChange={e => setNome(e.target.value)} />
-                    {nomeError && <small className="text-red-600">
-                        Nome inválido
-                    </small>}
+        <>
+            <div className="col-span-12 flex justify-between items-center">
+                <h1>Cadastro de Departamentos</h1>
+                <Button label="voltar" icon="pi pi-arrow-left" rounded onClick={() => navigate("/departamentos")} />
+            </div>
+            <div className="col-span-12">
+                <div className="flex w-full gap-2">
+                    <div className="w-2/3">
+                        <InputText placeholder="Nome" className="w-full" onChange={e => setNome(e.target.value)} />
+                        {nomeError && <small className="text-red-600">
+                            Nome inválido
+                        </small>}
+                    </div>
+                    <div className="w-1/3">
+                        <InputText placeholder="Sigla" className="w-full" onChange={e => setSigla(e.target.value)} />
+                        {siglaError && <small className="text-red-600">
+                            Sigla inválida
+                        </small>}
+                    </div>
+
                 </div>
-                <div className="w-1/3">
-                    <InputText placeholder="Sigla" className="w-full" onChange={e => setSigla(e.target.value)} />
-                    {siglaError && <small className="text-red-600">
-                        Sigla inválida
-                    </small>}
-                </div>
+                <Button label="Salvar" icon="pi pi-save" className="mt-2" onClick={inserir} />
 
             </div>
-            <Button label="Salvar" icon="pi pi-save" className="mt-2" onClick={inserir} />
-
-        </div>
+        </>
     )
 }
 
